8a6fcfed13e91ee986347b39920315b26232baf4
samba <keziah.samba@ilemgroup.com>
2024-12-19 15:44:54 +0100

    création endpoints de filtre : filterData

--- getData.js
+++ getData.js
@@ -2,11 +2,11 @@ import express from "express";
 import axios from "axios";
 import fs from "fs";
 
-// Création de l'application GetData pour récupérer la totalité des données de l'API
-const getData = express();
-
 const API_KEY = "e1039b3fe7762e9d6738cbdc81e0a6bb";
 const BASE_URL = `https://api.openweathermap.org/data/2.5/weather?lat=44.34&lon=10.99&appid=${API_KEY}`;
+
+// Création de l'application GetData pour récupérer la totalité des données de l'API
+const getData = express();
 
 // Définition de la route GET pour "/data_api"
 getData.get("/data_api", async (req, res) => {
